36200dc6f47af14c9a2fd2b981256fd1486b4dad
matveimug <madis.vaher@gmail.com>
2020-08-15 01:23:32 +0300

    unpkg

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-import * as THREE from './node_modules/three/build/three.module.js';
-import { OrbitControls } from './node_modules/three/examples/jsm/controls/OrbitControls.js';
-import { GLTFLoader } from './node_modules/three/examples/jsm/loaders/GLTFLoader.js';
+import * as THREE from 'https://unpkg.com/three/build/three.module.js';
+import { OrbitControls } from 'https://unpkg.com/three/examples/jsm/controls/OrbitControls.js';
+import { GLTFLoader } from 'https://unpkg.com/three/examples/jsm/loaders/GLTFLoader.js';
 
 
 const renderer = new THREE.WebGLRenderer( { antialias: true } );
